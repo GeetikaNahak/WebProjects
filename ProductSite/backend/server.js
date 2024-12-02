ead7e5870ceca85650e1954b2ea883c014bbf72d
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2024-12-02 20:13:34 +0530

    ProductSite

--- ProductSite/backend/server.js
+++ ProductSite/backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
-
+import dotenv from 'dotenv';
 const app = express();
-
+dotenv.config();
 app.get("/",(req,res)=>{
     res.send("H");
 })
-
+console.log(process.env.MONGO_URI);
+console.log("H")
 app.listen(3000,()=>{
     console.log("Server started at http://localhost:3000");
 })
